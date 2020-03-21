3ecdd6839b3ef7369462b13fa52e82381156dd8a
phimwadi <phimwadi0033@gmail.com>
2020-03-22 00:40:52 +0700

    commit7

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ function App() {
       id:2, name: "write node"
     }
   ])
+
+  const [ name,setName ] = useState([
+
+  ])
+
   useEffect( () => {
     retriveData()
   },[])
-
   const retriveData = () => {
     firestore.collection("tasks").onSnapshot( (snapshot) => {
       console.log(snapshot.docs)
@@ -25,7 +29,6 @@ function App() {
       setTasks(myTask)
     } )
   }
-
   const renderTask = () => {
     if (tasks && tasks.length)
       return tasks.map((task,index)=>{
@@ -36,9 +39,17 @@ function App() {
     else
         return (<li>No task</li>)
   }
+
+  const addTask = () => {
+    let id = tasks[tasks.length-1].id+1
+    firestore.collection("tasks").doc(id+'').set({id,name})
+  }
+
   return (
     <div >
         <h1>Todo</h1>
+        <input type="text" name="name" onChange={ (e)=> setName(e.target.value)}></input>
+        <button onClick={addTask}>Submit</button>
         <ul>{ renderTask() }</ul>
     </div>
   );
