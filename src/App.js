38c6d720f1e643ccb1eda11978dab085aa5de015
phimwadi <phimwadi0033@gmail.com>
2020-03-22 00:05:40 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks,setTasks] = useState([
+    { 
+      id:1, name: "do homework"
+    },
+    {
+    id:2, name: "write node"
+    }
+  ])
+  const renderTask = () => {
+    return tasks.map((task,index)=>{
+        return(
+          <li key={index}> {task.id} : {task.name}</li>
+        )
+      })
+  }
+
   return (
-    <div className="App">
-     Phimwadi
+    <div >
+        <h1>Todo</h1>
+        <ul>{ renderTask() }</ul>
     </div>
   );
-}
-
-export default App;
+}export default App
