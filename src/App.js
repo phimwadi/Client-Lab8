fdf5fba91c6299dbd1d991853c33e9f6f4102d61
phimwadi <phimwadi0033@gmail.com>
2020-03-22 00:25:28 +0700

    commit5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import './App.css';
+import {firestore} from './index'
 
 function App() {
   const [tasks,setTasks] = useState([
@@ -8,30 +9,33 @@ function App() {
     },
     {
       id:2, name: "write node"
-        
     }
   ])
 
   useEffect( () => {
+    retriveData()
+  },[])
 
-  })
-
+  const retriveData = () => {
+    firestore.collection("tasks").onSnapshot( (snapshot) => {
+      console.log(snapshot)
+    } )
+  }
   const renderTask = () => {
     if (tasks && tasks.length)
-    return tasks.map((task,index)=>{
-        return(
-          <li key={index}> {task.id} : {task.name}</li>
-        )
-      })
-  else
-      return (<li>No task</li>)
-}
-
-return (
-  <div >
-  <h1>Todo</h1>
-  <ul>{ renderTask() }</ul>
-</div>
-);
+      return tasks.map((task,index)=>{
+          return(
+            <li key={index}> {task.id} : {task.name}</li>
+          )
+        })
+    else
+        return (<li>No task</li>)
+  }
+  return (
+    <div >
+        <h1>Todo</h1>
+        <ul>{ renderTask() }</ul>
+    </div>
+  );
 }
 export default App;
