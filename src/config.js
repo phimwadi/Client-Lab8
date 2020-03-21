5354c53ca6b5c2fe5f660d430a26cec446c35555
phimwadi <phimwadi0033@gmail.com>
2020-03-22 00:17:10 +0700

    commit4

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyD_yr9oZBykRg5enDDVmq77NtGN8QBOvJU",
+  authDomain: "todo-46d82.firebaseapp.com",
+  databaseURL: "https://todo-46d82.firebaseio.com",
+  projectId: "todo-46d82",
+  storageBucket: "todo-46d82.appspot.com",
+  messagingSenderId: "729086905131",
+  appId: "1:729086905131:web:381d6f6811000e8eb5536e",
+  measurementId: "G-5G4L65E2FF"
+  };
+export default config; 
