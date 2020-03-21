379f8063f92a84a5681d241583d8ac57307bb9a8
phimwadi <phimwadi0033@gmail.com>
2020-03-22 00:11:28 +0700

    commit3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -7,21 +7,31 @@ function App() {
       id:1, name: "do homework"
     },
     {
-    id:2, name: "write node"
+      id:2, name: "write node"
+        
     }
   ])
+
+  useEffect( () => {
+
+  })
+
   const renderTask = () => {
+    if (tasks && tasks.length)
     return tasks.map((task,index)=>{
         return(
           <li key={index}> {task.id} : {task.name}</li>
         )
       })
-  }
+  else
+      return (<li>No task</li>)
+}
 
-  return (
-    <div >
-        <h1>Todo</h1>
-        <ul>{ renderTask() }</ul>
-    </div>
-  );
-}export default App
+return (
+  <div >
+  <h1>Todo</h1>
+  <ul>{ renderTask() }</ul>
+</div>
+);
+}
+export default App;
